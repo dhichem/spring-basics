0981016a1d8a7fdf00e3a391f51407b47a9ae1f2
dhichem <hichem.dimassi@gmail.com>
2024-11-09 18:33:01 +0100

    implemented swagger API documentation by integrating SpringDoc open-api also adding more description to API's by using swagger annotations

--- src/main/java/com/milkiyti/property_management/controller/UserController.java
+++ src/main/java/com/milkiyti/property_management/controller/UserController.java
@@ -2,6 +2,11 @@ package com.milkiyti.property_management.controller;
 
 import com.milkiyti.property_management.dto.UserDTO;
 import com.milkiyti.property_management.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -20,7 +25,13 @@ public class UserController {
         return new ResponseEntity<>(userService.register(userDTO), HttpStatus.OK);
     }
 
-    @PostMapping("/login")
+    @Operation(summary = "user login")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Login successfull",
+                content = { @Content(mediaType = "application/json",
+                        schema = @Schema(implementation = UserDTO.class)) }),
+        @ApiResponse(responseCode = "400", description = "Email or Password incorrect") })
+    @PostMapping(path = "/login", consumes = {"application/json"}, produces = {"application/json"})
     public ResponseEntity<?> login(@RequestBody UserDTO userDTO) {
         UserDTO ud = userService.login(userDTO.getEmail(), userDTO.getPassword());
         return new ResponseEntity<>(userService.login(userDTO.getEmail(), userDTO.getPassword()), HttpStatus.OK);
